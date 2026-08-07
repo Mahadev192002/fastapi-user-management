b25cc07d489d82cfdfdc4594a0ffffa72617c591
Mahadev192002 <mahadevd2002@gmail.com>
2026-08-07 20:30:00 +0530

    alter the user.py file bcz of s3

--- config.py
+++ config.py
@@ -8,10 +8,20 @@ class Settings(BaseSettings): # Pydantic model for application settings,
         env_file=".env",
         env_file_encoding="utf-8",
     ) # Configuration to specify the .env file and its encoding for loading environment variables
+    
+    
+  
 
     secret_key: SecretStr # Secret key used for cryptographic operations, such as signing JWT tokens, loaded from environment variables
     algorithm: str = "HS256" # Algorithm used for cryptographic operations, such as signing JWT tokens, with a default value of "HS256"
-    access_token_expire_minutes: int = 30 # Expiration time for access tokens in minutes, with a default value of 30 minutes
+    access_token_expire_minutes: int = 30 # Expiration time for access tokens in minutes, with a default value of 30 minutes 
+    
+    # S3 Configuration
+    s3_bucket_name: str
+    s3_region: str = "us-east-1"
+    s3_access_key_id: SecretStr | None = None
+    s3_secret_access_key: SecretStr | None = None
+    s3_endpoint_url: str | None = None
     
     max_upload_size_bytes: int = 5 * 1024 * 1024
     
